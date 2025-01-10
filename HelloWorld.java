9c8e8c4e62b16e359b4c336fbdcc5e65aacfbc87
Yashrajpawar-dev <yashrajpawar9604@gmail.com>
2025-01-10 21:19:58 +0530

    Added some info about git commands and about some nameings of casses and functions

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,3 +9,22 @@ public class HelloWorld {
 AddTwoNumberS --> Pascal naming convection
 addTwoNumbers --> Camel case naming convection
 */
+
+
+/* Git Commands
+
+
+git init
+sit status
+git add .
+git commit -a "sample commit"
+git remote add origin Url
+git push origin master
+
+-- Adding a commit --
+
+git status
+git add
+git commit -m "message"
+git push
+*/
