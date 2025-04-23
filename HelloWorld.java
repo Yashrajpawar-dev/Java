13c50a7ce7881dc732fcca04df7ec6f224396a83
Yashrajpawar-dev <yashrajpawar9604@gmail.com>
2025-04-23 13:30:40 +0530

    Initial upload of Java learning journey

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+/* Nameing convections in Java
+AddTwoNumberS --> Pascal naming convection for Classes
+addTwoNumbers --> Camel case naming convection for Function
+*/
+
+
+/* Git Commands
+
+
+git init
+git status
+git add .
+git commit -a "sample commit"
+git remote add origin Url
+git push origin master
+
+-- Adding a commit --
+
+git status
+git add
+git commit -m "message"
+git push
+*/
---
+++ set1.java
@@ -0,0 +1,77 @@
+// /*
+//  Write a program to calculate percentage of a student in CBSE board exam.
+//  His marks from 5 subjects must be taken as input from the keyboard
+// (Marks are out of 100) */
+
+
+
+
+// import java.util.Scanner;
+
+// public class Q1 {
+//   public static void main(String[] args) {
+//     Scanner s = new Scanner(System.in);
+
+//     int sub1, sub2, sub3, sub4, sub5,  SumOFAll;
+//     double percentage;
+
+
+//     System.out.println("Enter the marks for subject 1: ");
+//     sub1 = s.nextInt();
+//     System.out.println("Enter the marks for subject 2: ");
+//     sub2 = s.nextInt();
+//     System.out.println("Enter the marks for subject 3: ");
+//     sub3 = s.nextInt();
+//     System.out.println("Enter the marks for subject 4: ");
+//     sub4 = s.nextInt();
+//     System.out.println("Enter the marks for subject 5: ");
+//     sub5 = s.nextInt();
+
+//     SumOFAll = sub1 + sub2 + sub3 + sub4 + sub5;
+
+//     percentage = ((double) SumOFAll/500)*100;
+
+    
+//     System.out.println("Percentage of student according to the above marks");
+//     System.out.println(percentage);
+//     s.close();
+// }
+   
+// }
+
+// write a java program to add three numbers
+
+// public class Se1{
+//     public static void main (String[] args){
+//         int a = 10;
+//         int b = 20;
+//         double c = 634.6473;
+//         double sum = a + b + c;
+//         System.out.println("The sum of all three number is: " + sum);
+//     }
+// }
+
+
+// write a java program to calculate CGPA of a student
+// public class set1{
+//         public static void main (String[] args){
+//  float subject1 = 94;
+//  float subject2 = 84;
+// float subject3 = 86;
+// float CGPA = (subject1 + subject2 + subject3) / 30;
+// System.out.println(CGPA);        }
+//     }
+
+
+// write a java program which asks the user enter his/her name and greet them with "Hello <name>" , have good day" text
+
+
+// import java.util.Scanner;
+// public class set1{
+//               public static void main (String[] args){
+// Scanner S = new Scanner(System.in);
+// System.out.println("Enter your name:");
+// String name = S.next();
+// System.out.println("Hello " + name + ", have a good day"); 
+// S.close();
+//               }}
